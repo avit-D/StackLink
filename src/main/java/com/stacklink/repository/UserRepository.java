dca67c1b7697fd458eaacb0291694b10194e6ae3
khg1109 <161204128+khg1109@users.noreply.github.com>
2026-06-02 15:29:33 +0900

    JWT 기반 인증 구조 및 User 엔티티 수정

--- src/main/java/com/stacklink/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.stacklink.repository;
-
-import com.stacklink.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository
-        extends JpaRepository<User, Long> {
-}
