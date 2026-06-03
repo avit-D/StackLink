90819006ee64c2d620ec64a79e47dab01cb95ba9
buzichae <chaerin0605@naver.com>
2026-06-03 10:14:38 +0900

    [feat].main이랑 병합 -> 오류 해결

--- src/main/java/com/stacklink/domain/project/repository/ProjectRepository.java
+++ src/main/java/com/stacklink/domain/project/repository/ProjectRepository.java
@@ -3,5 +3,11 @@ package com.stacklink.domain.project.repository;
 import com.stacklink.domain.project.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+    List<Project> findByIsDeletedFalse();
+
+    Optional<Project> findByIdAndIsDeletedFalse(Long id);
 }
